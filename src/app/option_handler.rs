81e02671c33e7fe54130d55da49ecba7fea51f35
KilliBruhh <killian.serluppens@gmail.com>
2026-01-22 19:40:40 +0100

    fix: syntax error fix

--- src/app/option_handler.rs
+++ src/app/option_handler.rs
@@ -9,7 +9,6 @@ pub enum CommandList {
 
 pub fn call_option_handler(option_string: &String) -> Vec<String> {         // TODO - Change Return type
     println!("## Option String {}", option_string);
-    let 
     let option_list: Vec<String> = option_string.split(" ").map(String::from).collect();
 
     println!("## Option List  {:?}", option_list);
