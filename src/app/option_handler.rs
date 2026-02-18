97bd6bea490139d4ee5c7d285b4e172d364b9f84
KilliBruhh <killian.serluppens@gmail.com>
2026-02-18 21:15:24 +0100

    sync: push to sync

--- src/app/option_handler.rs
+++ src/app/option_handler.rs
@@ -10,8 +10,18 @@ pub enum CommandList {
 
 pub fn call_option_handler(option_string: &String) -> Vec<String> {         // TODO - Change Return type
     println!("## Option String {}", option_string);
-    let option_list: Vec<String> = option_string.split(" ").map(String::from).collect();
-
+    let option_list: Vec<String> = option_string
+        .split_whitespace()
+        .collect::<Vec<&str>>() // Temporarily collect to use chunks
+        .chunks(2)
+        .map(|chunk| chunk.join(" ")) // Joins "-p" and "8080" back together
+        .collect();
     println!("## Option List  {:?}", option_list);
     option_list
+}
+
+fn filter_option_list(option_list: &Vec<String>) {
+    for option in option_list {
+        if option.contains("-") {}
+    }
 }
