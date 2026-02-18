4a5b904cff79d9f2d678c52ba44641394bee50bc
KilliBruhh <killian.serluppens@gmail.com>
2026-02-18 23:50:34 +0100

    imp: better option detection (will still detect flag with space between -)

--- src/app/option_handler.rs
+++ src/app/option_handler.rs
@@ -12,30 +12,35 @@ pub fn call_option_handler(option_string: &String) -> Vec<String> {         // T
     println!("## Option String {}", option_string);
     let option_list: Vec<String> = option_string
         .split_whitespace()
-        .collect::<Vec<&str>>() // Temporarily collect to use chunks
+        .collect::<Vec<&str>>()
         .chunks(2)
-        .map(|chunk| chunk.join(" ")) // Joins "-p" and "8080" back together
+        .filter(|pair| {
+            pair.len() == 2 && pair[0].starts_with('-') && !pair[1].starts_with('-')
+        })
+        .map(|pair| pair.join(" "))
         .collect();
+
     println!("## Option List  {:?}", option_list);
+    detect_options(&option_list);
     option_list
 }
 
-fn filter_option_list(option_list: &Vec<String>) {
+fn detect_options(option_list: &Vec<String>) {
     for option in option_list {
-        if option.contains("-") {}
-    }
-}
-
-
-fn detect_options(option_list: Vec<String>) {
-    for option in option_list {
-        println!("## Option {}", option);
+        println!("\n## Option {}", option);
         if option.starts_with("-") {
+            println!("### Match Option: {}", option);
             // Option type
             // step 2 detect the option parameter (next in line)
 
         } else {
             // non type -> exit
+            println!("### Unknown Match Option: {}", option);
         }
     }
+}
+
+fn check_if_option_is_valid(option: &String) -> bool {
+    //option must be -<OPTION_NAME> <PARAMETER>
+    return false
 }
