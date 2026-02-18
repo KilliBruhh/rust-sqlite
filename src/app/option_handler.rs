ef1b94b3dc97873d2bbdb21f1d12a0ec43ca8e20
KilliBruhh <killian.serluppens@gmail.com>
2026-02-18 22:34:44 +0100

    sync: pc sync

--- src/app/option_handler.rs
+++ src/app/option_handler.rs
@@ -9,9 +9,22 @@ pub enum CommandList {
 }
 
 pub fn call_option_handler(option_string: &String) -> Vec<String> {         // TODO - Change Return type
-    println!("## Option String {}", option_string);
+    println!("## Option String [ {} ]", option_string);
     let option_list: Vec<String> = option_string.split(" ").map(String::from).collect();
 
     println!("## Option List  {:?}", option_list);
     option_list
+}
+
+fn detect_options(option_list: Vec<String>) {
+    for option in option_list {
+        println!("## Option {}", option);
+        if option.starts_with("-") {
+            // Option type
+            // step 2 detect the option parameter (next in line)
+
+        } else {
+            // non type -> exit
+        }
+    }
 }
