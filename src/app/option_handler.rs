3e25c9f09280804c828bb140c61b2390e45623bd
Killian Serluppens <killianserluppens@clubit.be>
2026-03-04 21:47:53 +0100

    imp: validation impromevents

--- src/app/option_handler.rs
+++ src/app/option_handler.rs
@@ -9,34 +9,30 @@ pub enum CommandList {
 }
 
     // INPUT test1 -t test1 -t test2 - t test3 -t test4 -tt test5
+    // test1 -t test1 -t test2 - test 3 -t test
 pub fn call_option_handler(option_string: &String) -> Vec<String> {         // TODO - Change Return type
     println!("## Option String {}", option_string);
-
-//        1. Slide through tokens pairwise (not non-overlapping chunks)
-//        2. Check if token[i] matches -X pattern (dash + single letter only)
-//        3. And token[i+1] is NOT an option (doesn't start with -)
-//        4. Then combine them
 
     let option_pairings: Vec<String> = option_string
         .split_whitespace()
         .collect::<Vec<&str>>()
         .chunks(2)
         .filter(|pair| {
-            println!("### Pairing [{} {}]", pair[0], pair[1]);
-            if (pair.len() == 2 && pair[0].starts_with('-')  && pair[0].len() > 1 && !pair[1].starts_with('-')) {
-                println!("-> TRUE");
-            } else if (pair[0].contains(' ') || pair[1].contains(' '))  {
-                println!("-> FALSE 1");
-            } else {
-                println!("-> NO DETECT");
-            }
+            println!("## Pair[0] {}", pair[0]);
+            println!("## Pair[1] {}", pair[1]);
 
+            if (pair[0].starts_with("-")) && (!pair[1].starts_with("-") && pair[0].len() == 2 ) {
+                println!("--V PASS")
+            } else {
+                println!("--X NO PASS");
+            }
+            println!(" ");
             return pair[0] == option_string;
         })
         .map(|pair| pair.join(" "))
         .collect();
-    println!("## Option List  {:?}", option_pairings);
-    option_pairings
+        println!("## Option List  {:?}", option_pairings);
+        option_pairings
 }
 
 
